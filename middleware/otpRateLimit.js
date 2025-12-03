04e6ca5bd65b85dc23b58e897bfee02b86de9cd0
surajkumar0011 <edugaon9264@gmail.com>
2025-12-03 12:45:01 +0530

    some code add in api

--- middleware/otpRateLimit.js
+++ middleware/otpRateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const otpLimiter = rateLimit({
   windowMs: 60 * 1000, 
-  max: 10,
+  max: 20,
   message: { success: false, message: "Too many OTP requests" },
 });
 
